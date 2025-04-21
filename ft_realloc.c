d3a971456beeb9bed8563ace431bb10f129171a0
Nico <nlewicki@student.42heilbronn.de>
2025-04-21 13:06:26 +0200

    updated ft_realloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:48:55 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/23 15:02:13 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/04/21 13:05:14 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+void	*ft_realloc(void *ptr, size_t new_size)
 {
-	void	*new_ptr;
-	size_t	copy_size;
-
+	void *new_ptr;
+	size_t copy_size;
 	if (new_size == 0)
-		return (free(ptr), NULL);
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-	if (ptr)
-	{
-		if (old_size < new_size)
-			copy_size = old_size;
-		else
-			copy_size = new_size;
-		ft_memcpy(new_ptr, ptr, copy_size);
-		free(ptr);
-	}
+	// Copy as much as possible; assume ptr was allocated with enough space
+	// This is a simplification since we can't know the old size
+	copy_size = new_size; // This is a compromise; see notes below
+	ft_memcpy(new_ptr, ptr, copy_size);
+	free(ptr);
 	return (new_ptr);
 }
