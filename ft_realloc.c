9a85d64bf25b4facac574ca7025db75492501f09
Nico <nlewicki@student.42heilbronn.de>
2025-05-27 13:24:10 +0200

    changed realloc to a more consistent way of handleing our adding only 1 element every call

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:48:55 by nlewicki          #+#    #+#             */
-/*   Updated: 2025/05/13 12:17:37 by nlewicki         ###   ########.fr       */
+/*   Updated: 2025/05/27 13:23:33 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*ft_realloc(void *ptr, size_t new_size)
 {
 	void	*new_ptr;
-	size_t	copy_size;
+	size_t	old_size;
 
 	if (new_size == 0)
 	{
@@ -24,11 +24,11 @@ void	*ft_realloc(void *ptr, size_t new_size)
 	}
 	if (!ptr)
 		return (malloc(new_size));
-	new_ptr = malloc(new_size);
+	new_ptr = ft_calloc(1, new_size);
 	if (!new_ptr)
 		return (NULL);
-	copy_size = new_size;
-	ft_memcpy(new_ptr, ptr, copy_size);
+	old_size = new_size - (new_size / (2));
+	ft_memcpy(new_ptr, ptr, old_size);
 	free(ptr);
 	return (new_ptr);
 }
